afdd384f575a48dfc67aa517d94d5a3ef535d0dd
Vera Sticker <svsticker@gmail.com>
2017-03-29 22:26:08 -0400

    alter color change mechanics

--- js/weather.js
+++ js/weather.js
@@ -5,9 +5,24 @@ $( document ).ready(function() {
                  $.ajax({
                      url: "http://api.openweathermap.org/data/2.5/weather?lat="+position.coords.latitude+"&lon="+position.coords.longitude+"&appid=0cd024506296321d372539d62a64351c",
                      success: function(response){
-                         console.log(response);
                          $("#lbox").remove();
                          $("main").html("<h2>"+response.name+"</h2><hr><p>"+Math.round(kelToFar(response.main.temp))+"<a href='#f'>F&deg;</a>|"+Math.round(kelToCel(response.main.temp))+"<a href='#c'>C&deg;</a></p>");
+                         
+                         if (response.main.temp >= 305.372) {
+                             $("body").css("background-color", "red");
+                         } else if (response.main.temp >= 299.817) {
+                             $("body").css("background-color", "orange");
+                         } else if (response.main.temp >= 294.261) {
+                             $("body").css("background-color", "yellow");
+                         } else if (277.595 <= response.main.temp < 294.261) {
+                             $("body").css("background-color", "green");
+                         } else if (response.main.temp <= 277.594) {
+                             $("body").css("background-color", "deepskyblue");
+                         } else if (response.main.temp <= 273.15) {
+                             $("body").css("background-color", "cornflowerblue");
+                         } else if (response.main.temp <= 266.483) {
+                             $("body").css("background-color", "blue");
+                         };
                      }
                  });             
              
